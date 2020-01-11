7567721401fc0f266b61a029f6e61eaddab87804
Luciano Liboni <luciano_liboni@hotmail.com>
2020-01-10 21:30:23 -0300

    MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,14 +10,6 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    /*@Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-    */
     @Override
     public void start(Stage primaryStage) {
         try {
